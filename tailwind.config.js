764f3f4fccb2cefa51f31e9f1044fc164a73e3b9
ABIN S <157473525+AbinSurendran@users.noreply.github.com>
2024-07-18 11:42:12 +0530

    Update tailwind.config.js
    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -26,7 +26,7 @@ module.exports = {
       colors: {
         primary: "#1c1c22",
         accent: {
-          DEFAULT: "#039be5",
+          DEFAULT: "#00ff99",
           hover: "#00e187",
         },
       },
